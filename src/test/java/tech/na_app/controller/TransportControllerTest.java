eb5b5aeb3b7325122d8c17fb75b4ceb694fe2f21
ihor.shmat <ihor.shmat@mustpay.tech>
2022-11-21 22:11:00 +0200

    add test package

---
+++ src/test/java/tech/na_app/controller/TransportControllerTest.java
@@ -0,0 +1,13 @@
+package tech.na_app.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransportControllerTest {
+
+    @Test
+    void saveNewUser() {
+
+    }
+}
---
+++ src/test/java/tech/na_app/services/transport/TransportServiceTest.java
@@ -0,0 +1,12 @@
+package tech.na_app.services.transport;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransportServiceTest {
+
+    @Test
+    void saveNewTransport() {
+    }
+}
