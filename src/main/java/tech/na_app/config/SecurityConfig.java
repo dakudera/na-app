15509749a5801fd78fa5db6601e3606906c6999b
Ponomarenko Dmitrij <grinjou906@gmail.com>
2022-11-29 15:41:43 +0200

    save

--- src/main/java/tech/na_app/config/SecurityConfig.java
+++ src/main/java/tech/na_app/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
                 .antMatchers("/v3/api-docs/**").permitAll()
