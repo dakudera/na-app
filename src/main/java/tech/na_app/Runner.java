9e4fa85dd9b2c9080acc05b42419efc7492d23c6
dakudera <d.ponomarenkoa@gmail.com>
2024-07-22 16:09:34 +0300

    save maven wrapper and dockerfile

--- src/main/java/tech/na_app/Runner.java
+++ src/main/java/tech/na_app/Runner.java
@@ -2,19 +2,12 @@ package tech.na_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Runner extends SpringBootServletInitializer {
+public class Runner {
 
     public static void main(String[] args) {
         SpringApplication.run(Runner.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Runner.class);
     }
 
 }
